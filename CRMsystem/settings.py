8b617dcc32c4e6c28140a8df216790f7d61b16a4
Gulraiz <gulraiznoorbari@gmail.com>
2021-09-30 14:41:14 +0500

    Adding config for Heroku-Postgresql connection

--- CRMsystem/settings.py
+++ CRMsystem/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -95,6 +96,8 @@ DATABASES = {
     # }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
