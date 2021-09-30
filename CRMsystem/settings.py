dc5c1f3513e082806949d81db3dbda8242727f98
Gulraiz <gulraiznoorbari@gmail.com>
2021-09-30 14:20:25 +0500

    Fixed Error-500

--- CRMsystem/settings.py
+++ CRMsystem/settings.py
@@ -146,6 +146,8 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
