207b2dc5a77050801aff89c048d46af1d2479e53
Gulraiz <gulraiznoorbari@gmail.com>
2021-09-30 18:04:26 +0500

    minor fixes

--- CRMsystem/urls.py
+++ CRMsystem/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
+
+urlpatterns += [static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)]
