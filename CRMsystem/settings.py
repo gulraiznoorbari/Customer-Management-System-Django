2100d2cdc9cf829a9ce6ac8b5706c8476c75c921
Gulraiz <gulraiznoorbari@gmail.com>
2021-09-30 17:37:49 +0500

    minor fixes

--- CRMsystem/settings.py
+++ CRMsystem/settings.py
@@ -138,8 +138,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+# MEDIA = '/images/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
