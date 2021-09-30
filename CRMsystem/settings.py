5781d1d578b5ecd24926c198d6985bd78793ccc8
Gulraiz <gulraiznoorbari@gmail.com>
2021-09-30 15:07:29 +0500

    Modified settings.py file

--- CRMsystem/settings.py
+++ CRMsystem/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static') 
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
